89165afb588930fa792acc19e7f341923998279c
prado156 <ppattem@purdue.edu>
2022-09-29 11:45:48 -0400

    finished setting up client, server, and mongodb

---
+++ server/app.js
@@ -0,0 +1,35 @@
+// import modules
+
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+
+//app
+const app = express();
+
+// mongodb
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB Not Connected"));
+
+// middleware
+app.use(morgan("dev"));
+app.use(cors({ origin: true, credentials: true }));
+
+// routes
+// const testRoutes = require("./routes/test");
+// app.use("/", testRoutes);
+
+// port
+const port = process.env.PORT || 8080;
+
+//listener
+const server = app.listen(port, () =>
+  console.log(`Server is running on ${port}`)
+);
