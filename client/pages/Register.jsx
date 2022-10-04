d7d672f126b3ac0d620ec848e3a5e1d494a589e8
Ryan Chang <chang774@purdue.edu>
2022-10-03 23:08:16 -0400

    Added registration page

---
+++ client/pages/Register.jsx
@@ -0,0 +1,152 @@
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+import { LockClosedIcon } from "@heroicons/react/20/solid";
+import Input from "../components/Input";
+import SignIn from "./btn_google_signin_dark_normal_web@2x.png";
+import SignIn2 from "./btn_google_signin_dark_pressed_web@2x.png";
+
+
+export default function Example() {
+  return (
+
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-gray-50">
+        <body class="h-full">
+        ```
+      */}
+
+      <div class="flex items-center justify-center min-h-screen bg-gray-100">
+        <div class="px-8 py-6 mt-4 rounded-lg text-left bg-white shadow-lg">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            />
+            {/* <h1 className=" mx-auto h-12 w-auto" alt="Your Company">
+              Enjoyagoals
+            </h1> */}
+            <h1 className="mt-6 text-center text-3xl font-bold tracking-tight text-red-900">
+              Welcome to Enjoyagoals!
+            </h1>
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Register for a free account
+            </h2>
+
+
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Username
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Username"
+                />
+              </div>
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Confirm Password"
+                />
+              </div>
+
+              <div class="border-t border-gray-200 pt-4">
+
+              </div>
+
+            </div>
+
+
+
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+
+                Create Account
+              </button>
+              <div>
+                <p className="mt-2 text-center text-sm text-gray-600">
+                  <b>Or</b>
+                </p>
+              </div>
+              &nbsp;
+              <div>
+
+                <img
+                  className="mx-auto h-12 w-auto"
+                  src={SignIn} onclick="this.src='{SignIn2}'"
+                  alt="Google"
+
+                />
+
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
