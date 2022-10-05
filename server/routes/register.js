c039ebc6f1c3f327068e53f638a746911558b272
Nick Norton <nortonn@purdue.edu>
2022-10-05 15:06:32 -0400

    messy checkpoint, post request works, need to be able to handle request fields
    this can receive a POST request and then put hard-coded data into the database, but it cannot read data sent in the request and put that into the database

--- server/routes/register.js
+++ server/routes/register.js
@@ -4,13 +4,15 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
+/*
+router.get('/register', function(req, res) {
     res.send("register get works, idk about post though");
 });
+*/
 
-/*
 router.post("/register", async (req, res) => {
     const user = req.body;
+    /*
 
     const takenUsername = await User.findOne({username: user.username});
     const takenEmail = await User.findOne({email: user.email});
@@ -29,16 +31,24 @@ router.post("/register", async (req, res) => {
         dbUser.save();
         res.json({message: "Success"});
     }
+    */
 
+    /*
     const dbUser = new User({
         username: user.username.toLowerCase(),
-            email: user.email.toLowerCase(),
-            password: user.password
-        })
-
-        dbUser.save();
-        res.json({message: "Success"});
+        email: user.email.toLowerCase(),
+        password: user.password
     })
-*/
+    */
+
+    const dbUser = new User({
+        username: "testNorton",
+        email: "nortonn@purdue.edu",
+        password: "oooSneakyPassword"
+    });
+
+    dbUser.save();
+    res.json({message: "Success"});
+})
 
 module.exports = router;
