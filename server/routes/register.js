6849a12d9aec38d9b23cc9d6615dcdc855eadb83
Nick Norton <nortonn@purdue.edu>
2022-10-04 21:41:53 -0400

    register route code *NEEDS TO BE TESTED*

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,7 +1,27 @@
+const User = require('../models/UserModel');
+
 var router = require('express').Router();
 
-router.get('/', function(req, res) {
-    res.send('registration page');
-});
+router.post("/register", async (req, res) => {
+    const user = req.body;
+
+    const takenUsername = await User.findOne({username: user.username});
+    const takenEmail = await User.findOne({email: user.email});
+
+    if (takenUsername || takenEmail) {
+        res.json({message: "Username or email has already been taken"});
+    } else {
+        user.password = await bcrypt.hash(req.body.password, 10);
+
+        const dbUser = new User({
+            username: user.username.toLowerCase(),
+            email: user.email.toLowerCase(),
+            password: user.password
+        })
+
+        dbUser.save();
+        res.json({message: "Success"});
+    }
+})
 
 module.exports = router;
