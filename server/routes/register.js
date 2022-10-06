470b7f10256417d6d223bcdeb756404402287de9
Nick Norton <nortonn@purdue.edu>
2022-10-05 21:41:05 -0400

    checking for taken username/email, password hashing working!

--- server/routes/register.js
+++ server/routes/register.js
@@ -15,14 +15,23 @@ router.use(express.json());
 router.post("/", async (req, res) => {
     const user = req.body;
 
-    const dbUser = new User({
-        username: user.username.toLowerCase(),
-        email: user.email.toLowerCase(),
-        password: user.password
-    })
+    const takenUsername = await User.findOne({username: user.username});
+    const takenEmail = await User.findOne({email: user.email});
 
-    dbUser.save();
-    res.json({message: "Success"});
-})
+    if (takenUsername || takenEmail) {
+        res.json({message: "Username or email has already been taken"});
+    } else {
+        user.password = await bcrypt.hash(req.body.password, 10);
+
+        const dbUser = new User({
+            username: user.username.toLowerCase(),
+            email: user.email.toLowerCase(),
+            password: user.password
+        })
+
+        dbUser.save();
+        res.json({message: "Success"});
+    }
+});
 
 module.exports = router;
