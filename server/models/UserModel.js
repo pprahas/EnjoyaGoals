9198b97eaa3c7eb77037f296b172946451d7d15c
Nick Norton <nortonn@purdue.edu>
2022-10-06 20:48:07 -0400

    Registration backend: added first name and last name fields

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -5,6 +5,14 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
     },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
     email: {
         type: String,
         required: true,
