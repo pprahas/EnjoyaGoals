901c07d2c43bf67dc9134f167f34fb9f307d4fff
Nick Norton <nortonn@purdue.edu>
2022-10-08 21:16:04 -0400

    database queries no longer return passwords; also can query specific fields now

--- server/routes/query_database.js
+++ server/routes/query_database.js
@@ -5,15 +5,60 @@ const mongoose = require("mongoose");
 const router = express.Router();
 router.use(express.json());
 
-router.get("/", function (req, res) {
-    User.find((err, docs) => {
-        if (!err) {
-            res.send(docs);
-        } else {
-            console.log(err);
-            res.send({ status: "error" });
-        }
-    });
+// query all User data in the database (except for passwords)
+router.get("/", async (req, res) => {
+    try {
+        const findRes = await User.find().select("-password");
+        
+        res.send(findRes);
+    } catch (error) {
+        console.log(error);
+        res.send({ status: "error" });
+    }
+});
+
+// query all User usernames in the database
+router.get("/usernames", async (req, res) => {
+    try {
+        const findRes = await User.find().select("username");
+        res.send(findRes);
+    } catch (error) {
+        console.log(error);
+        res.send({ status: "error" });
+    }
+});
+
+// query all User emails in the database
+router.get("/emails", async (req, res) => {
+    try {
+        const findRes = await User.find().select("email");
+        res.send(findRes);
+    } catch (error) {
+        console.log(error);
+        res.send({ status: "error" });
+    }
+});
+
+// query all User firstNames in the database
+router.get("/firstNames", async (req, res) => {
+    try {
+        const findRes = await User.find().select("firstName");
+        res.send(findRes);
+    } catch (error) {
+        console.log(error);
+        res.send({ status: "error" });
+    }
+});
+
+// query all User lastNames in the database
+router.get("/lastNames", async (req, res) => {
+    try {
+        const findRes = await User.find().select("lastName");
+        res.send(findRes);
+    } catch (error) {
+        console.log(error);
+        res.send({ status: "error" });
+    }
 });
 
 module.exports = router;
