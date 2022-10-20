b94ef4a2e48bb2f46b73588e1cb06808fd5ca774
prado156 <ppattem@purdue.edu>
2022-10-20 13:09:10 -0400

    fixed leaderboard tezt

--- client/components/Leaderboard.jsx
+++ client/components/Leaderboard.jsx
@@ -3,14 +3,9 @@ import { FaFire, FaPoo } from "react-icons/fa";
 const Sidebar = () => {
   return (
     <div className="fixed w-28 overflow-auto top-0 right-0 h-screen w-20 flex flex-col bg-gray-900 text-white shadow-lg">
-      <SideBarIcon icon={<FaFire size="60" />} />
-      <SideBarIcon icon={<BsPlus size="60" />} />
-      <SideBarIcon icon={<BsFillLightningFill size="60" />} />
-      <SideBarIcon icon={<FaPoo size="60" />} />
-      <SideBarIcon icon={<BsGearFill size="60" />} />
-      <SideBarIcon icon={<BsGearFill size="60" />} />
-      <SideBarIcon icon={<BsGearFill size="60" />} />
-      <SideBarIcon icon={<BsGearFill size="60" />} />
+      <p className="text-center text-yellow-500 mt-8 text-lg font-serif font-semibold">
+        Leaderboard
+      </p>
     </div>
   );
 };
