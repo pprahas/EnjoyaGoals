40263b8fa1845ac13d133e5e02cf573bd41dcf78
prado156 <ppattem@purdue.edu>
2022-10-22 20:58:29 -0400

    trying to fix scrolling

--- client/components/CompletedTasks.jsx
+++ client/components/CompletedTasks.jsx
@@ -6,7 +6,11 @@ const CompletedTasks = (props) => {
   const AllTasks = [
     <Tasks date="10/22/2022" name="Fixing Frontend" points="30" />,
     <Tasks date="2/22/2022" name="Fixing Backend" points="10" />,
-    <Tasks date="10/22/2022" name="Frontend" points="90" />,
+    <Tasks date="110/22/2022" name="Frontend" points="90" />,
+    <Tasks date="110/22/2022" name="Frontend" points="90" />,
+    <Tasks date="110/22/2022" name="Frontend" points="90" />,
+    <Tasks date="110/22/2022" name="Frontend" points="90" />,
+    <Tasks date="10/29/2022" name="Fixing Homepage" points="20" />,
     <Tasks date="10/29/2022" name="Fixing Homepage" points="20" />,
   ];
 
@@ -15,13 +19,13 @@ const CompletedTasks = (props) => {
   }
 
   return (
-    <div className="overlay" class="fixed pin z-50 overflow-auto flex">
+    <div className="overlay" class="fixed pin z-50 flex">
       <div className="modal">
-        <div className="modal-content">
-          <h1 className="text-center text-6xl font-semibold pt-8 text-indigo-400	">
+        <div className="modal-content overflow-auto">
+          <h1 className="text-center text-6xl font-semibold pt-8 text-indigo-400">
             Completed Tasks
           </h1>
-          {AllTasks}
+          <div>{AllTasks}</div>
           <button
             type="button"
             className="mt-4 group relative flex w-full justify-center rounded-md border border-transparent bg-red-500 py-2 px-4 text-sm font-medium text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
