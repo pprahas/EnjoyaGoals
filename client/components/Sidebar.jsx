df280882e7145ee10810b84f139f60a4e173c2e4
prado156 <ppattem@purdue.edu>
2022-10-29 18:01:11 -0400

    fixed sidebar

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -1,16 +1,22 @@
 import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
 import { FaFire, FaPoo } from "react-icons/fa";
 const Sidebar = () => {
+  const createRoom = () => {
+    console.log("creating room");
+  };
+
   return (
     <div className="fixed w-32 overflow-auto top-0 left-0 h-screen w-20 flex flex-col bg-gray-900 text-white shadow-lg">
+      <div onClick={createRoom}>
+        <SideBarIcon icon={<BsPlus size="60" />} />
+      </div>
+
       <SideBarIcon icon={<FaFire size="60" />} />
-      <SideBarIcon icon={<BsPlus size="60" />} />
       <SideBarIcon icon={<BsFillLightningFill size="60" />} />
       <SideBarIcon icon={<FaPoo size="60" />} />
-      <SideBarIcon icon={<BsGearFill size="60" />} />
-      <SideBarIcon icon={<BsGearFill size="60" />} />
-      <SideBarIcon icon={<BsGearFill size="60" />} />
-      <SideBarIcon icon={<BsGearFill size="60" />} />
+      <a href="/profile">
+        <SideBarIcon icon={<BsGearFill size="60" />} />
+      </a>
     </div>
   );
 };
