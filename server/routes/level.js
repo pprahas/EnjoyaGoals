db8643774c9aca539612a0b6e1f83275c186fdbf
Nabi Nabiyev <69466681+mikewazovski@users.noreply.github.com>
2022-10-30 16:20:41 -0400

    Update level.js

--- server/routes/level.js
+++ server/routes/level.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Task = require("../models/TaskModel");
+const User = require("../models/UserModel");
+const Room = require("../models/RoomModel");
+
+
 const mongoose = require("mongoose");
 
 router.use(express.json());
-
 
 router.post("/assign", async (req, res) => {
     const taskToUpdate = await Task.findById(req.body.id);
@@ -12,14 +15,13 @@ router.post("/assign", async (req, res) => {
     const room = await Room.findById(req.body.roomId);
     const taskLevel = user.taskLevel;
     const taskPoints = taskToUpdate.points;
-    const roomPoints = room.points;
-    const taskName = taskToUpdate.name;
-    const roomName = room.name;
-    const taskLevelKey = roomName + taskName;
+    const taskLevelKey = room._id;
     const taskLevelValue = taskLevel.get(taskLevelKey);
     const newTaskLevelValue = taskLevelValue + taskPoints;
-    taskLevel.set(taskLevelKey, newTaskLevelValue);
+    const taskLevelValueTo100 = Math.floor(newTaskLevelValue / 100);
+    taskLevel.set(taskLevelKey, taskLevelValueTo100);
     user.taskLevel = taskLevel;
     user.save();
     return res.status(200).json({ msg: "Task assigned successfully." });
 });
+
