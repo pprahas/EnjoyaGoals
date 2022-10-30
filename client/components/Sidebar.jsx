4faafb5fd382633853bbc2bb75d377a2e43c4b39
prado156 <ppattem@purdue.edu>
2022-10-30 06:13:57 -0400

    user's rooms now show up

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -1,18 +1,29 @@
-import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import { BsPlus, BsGearFill } from "react-icons/bs";
 import { useState } from "react";
-import { FaFire, FaPoo } from "react-icons/fa";
+import { FaFire } from "react-icons/fa";
 import CreateRoom from "./CreateRoom";
 const Sidebar = () => {
   const [showCreateRoom, setShowCreateRoom] = useState(false);
-  const createRoom = () => {
-    console.log("creating room");
-  };
+  let user_object = window.localStorage.getItem("user_data");
+  user_object = JSON.parse(user_object);
+  let userRooms = user_object.rooms;
+
+  // creating a temp room object, this will get replaced by the room array in the user object
+  var obj = new Object();
+  obj.name = "Discord Room";
+  obj.type = "monarchy";
+  userRooms.push(obj);
+
+  const eachRoom = userRooms.map((d) => {
+    const roomName = Object.values(d)[0];
+    return <SideBarIcon icon={<FaFire size="60" />} text={roomName} />;
+  });
 
   return (
-    <div className="fixed w-32 overflow-auto top-0 left-0 h-screen w-20 flex flex-col bg-gray-900 text-white shadow-lg">
+    <div className="fixed w-32 overflow-auto top-0 left-0 h-screen w-20 flex flex-col bg-gray-900 text-white ">
       <div>
         <div onClick={() => setShowCreateRoom(true)}>
-          <SideBarIcon icon={<BsPlus size="60" />} />
+          <SideBarIcon icon={<BsPlus size="60" />} text="New Server" />
         </div>
 
         <CreateRoom
@@ -20,21 +31,19 @@ const Sidebar = () => {
           show={showCreateRoom}
         ></CreateRoom>
       </div>
-
-      <SideBarIcon icon={<FaFire size="60" />} />
-      <SideBarIcon icon={<BsFillLightningFill size="60" />} />
-      <SideBarIcon icon={<FaPoo size="60" />} />
+      {eachRoom}
+      {/* <SideBarIcon icon={<FaFire size="60" />} /> */}
       <a href="/profile">
-        <SideBarIcon icon={<BsGearFill size="60" />} />
+        <SideBarIcon icon={<BsGearFill size="60" />} text="Profile" />
       </a>
     </div>
   );
 };
 
-const SideBarIcon = ({ icon, text = "room" }) => (
+const SideBarIcon = ({ icon, text }) => (
   <div className="sidebar-icon group">
     {icon}
-    {/* <span class="sidebar-tooltip group-hover:scale-100">{text}</span> */}
+    <span class="sidebar-tooltip group-hover:scale-100">{text}</span>
   </div>
 );
 
