b8fdcf207af26efd074afc429fe34115c7186ae4
Ryan Chang <chang774@purdue.edu>
2022-10-31 19:05:53 -0400

    Implemented frontend calendar functionality
    Deadlines are marked in red circles on the calendar. Clicking each
    deadline brings up a list of all deadlines with the same date.

--- client/components/CalendarView.jsx
+++ client/components/CalendarView.jsx
@@ -4,6 +4,9 @@ import "./TaskModal.css";
 import Calendar from "react-calendar";
 import "./Calendar.css";
 import Tasks from "./Task";
+import Pending from "./Pending";
+
+import TempTasks from "./TempTasks"
 
 //import "react-calendar/dist/Calendar.css";
 
@@ -12,6 +15,7 @@ const CalendarView = (props) => {
     return null;
   }
   let completedTasks = [];
+  let deadlineTasks = [];
 
   var obj = new Object();
   obj.name = "task-name";
@@ -20,6 +24,7 @@ const CalendarView = (props) => {
   obj.deadline = "2022-10-03T00:00:00.000+00:00";
   obj.points = 34;
   obj.assigned = "prado156";
+  obj.status = "pending";
   completedTasks.push(obj);
 
   var obj2 = new Object();
@@ -29,6 +34,7 @@ const CalendarView = (props) => {
   obj2.deadline = "2022-10-05T00:00:00.000+00:00";
   obj2.points = 34;
   obj2.assigned = "prado156";
+  obj2.status = "pending";
   completedTasks.push(obj2);
 
 
@@ -39,34 +45,69 @@ const CalendarView = (props) => {
     const date = Object.values(d)[3];
     const points = Object.values(d)[4];
     const assigned = Object.values(d)[5];
+    const status = Object.values(d)[6];
+    // return <Tasks date={task.deadline} name={task.name} points={task.points} />;
 
-    return (
-      <Tasks
-        date={date}
-        desc={desc}
-        difficulty={difficulty}
-        assigned={assigned}
-        name={name}
-        points={points}
-      />
-    );
-  });
-  
+    if (status === "pending") {
+        return (
+            <Pending
+                date={date.slice(0,10)}
+                desc={desc}
+                difficulty={difficulty}
+                assigned={assigned}
+                name={name}
+                points={points}
+            />
+        );
+    }
+    //probably not needed
+    else if (status === "complete") {
+        return (
+            <Tasks
+                date={date.slice(0,10)}
+                desc={desc}
+                difficulty={difficulty}
+                assigned={assigned}
+                name={name}
+                points={points}
+            />
+        );
+    }
+});
+
   const marked = new Set()
-/*
-  const onSelect=(e)=>{
+
+  const [showAll, setShowAll] = useState(false);
+
+  const [taskArray, setTasks] = useState([]);
+
+  const onSelect = (e) => {
     const locale = 'fr-CA';
     const date = new Intl.DateTimeFormat(
-      locale, 
+      locale,
       {
-        year: "numeric", 
-        month: "2-digit", 
+        year: "numeric",
+        month: "2-digit",
         day: "2-digit"
       }).format(e)
-      console.log(date);
-      console.log(marked.has(date));
+    //console.log(date);
+    //console.log(marked.has(date));
+    if (!marked.has(date)) {
+      return;
+    } else {
+      let temp = [];
+      for (let val of eachTask.values()) {
+        // create new array
+        // if val.props.date === date, add obj to array
+        if (date === val.props.date) {
+          temp.push(val);
+        }
+      }
+      setTasks([temp]);
+      setShowAll(true);
+    }
   }
-  */
+
 
   return (
     <div className="overlay" class="fixed pin z-50 overflow-auto flex">
@@ -76,29 +117,34 @@ const CalendarView = (props) => {
             Calendar
           </h1>
           <div className="grid justify-items-center">
-            
+
             <Calendar
-            tileClassName={({ date }) => {
-              const locale = 'fr-CA';
-              const dateConv = new Intl.DateTimeFormat(
-                locale, 
-                {
-                  year: "numeric", 
-                  month: "2-digit", 
-                  day: "2-digit"
-                }).format(date)
+              tileClassName={({ date }) => {
+                const locale = 'fr-CA';
+                const dateConv = new Intl.DateTimeFormat(
+                  locale,
+                  {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit"
+                  }).format(date)
                 for (let val of eachTask.values()) {
-                  let deadline=val.props.date;
-                  if (!marked.has(deadline)){
-                    marked.add(deadline.slice(0,10));
+                  let deadline = val.props.date;
+                  if (!marked.has(deadline)) {
+                    marked.add(deadline);
                   }
                 }
-              if(marked.has(dateConv)){
-               return  'highlight'
-              }
-            }}
-        
-            //onClickDay={(e)=>onSelect(e)}
+                if (marked.has(dateConv)) {
+                  return 'highlight'
+                }
+              }}
+
+              onClickDay={(e) => onSelect(e)}
+            />
+            <TempTasks
+              onClose={() => setShowAll(false)}
+              show={showAll}
+              tasks={taskArray}
             />
           </div>
           {/* <h1 className="text-center text-8xl text-red-400	">Homepage</h1>; */}
