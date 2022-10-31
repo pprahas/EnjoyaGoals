170067c88eae4392c9079c7f9c11981148d813b9
Nick Norton <nortonn@purdue.edu>
2022-10-31 14:59:20 -0400

    added dummy room? maybe?

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -7,15 +7,43 @@ import JoinRoom from "./JoinRoom";
 const Sidebar = () => {
   const [showCreateRoom, setShowCreateRoom] = useState(false);
   const [showJoinRoom, setShowJoinRoom] = useState(false);
+  var dummyRoomJson = {
+    "_id": "635ece31760e04fe118bb4fd",
+    "name": "testRoom",
+    "owner": "635ecdd9760e04fe118bb4f9",
+    "type": "Democracy",
+    "users": [
+        "635ecdd9760e04fe118bb4f9"
+    ],
+    "__v": 46,
+    "assignedTasks": [],
+    "completedTasks": [],
+    "teamTasks": [],
+    "updatedAt": "2022-10-31T18:38:37.155Z"
+  }
+  dummyRoomJson = JSON.stringify(dummyRoomJson);
   let user_object = window.localStorage.getItem("user_data");
   user_object = JSON.parse(user_object);
+  user_object.rooms.push(dummyRoomJson);
+  console.log(user_object.rooms);
+  window.localStorage.setItem("user_data", JSON.stringify(user_object));
   let userRooms = user_object.rooms;
 
   // creating a temp room object, this will get replaced by the room array in the user object
-  var obj = new Object();
-  obj.name = "Discord Room";
-  obj.type = "monarchy";
+//  var obj = new Object();
+//  obj = JSON.parse(dummyRoomJson);
+
+/*
+  obj._id = "635ece31760e04fe118bb4fd";
+  obj.name = "testRoom";
+  obj.type = "Democracy";
+  obj.owner = "635ecdd9760e04fe118bb4f9";
+  obj.users = ["635ecdd9760e04fe118bb4f9", "63557b1340c7cd797a0ea840"];
+  obj.assignedTasks = [];
+  obj.completedTasks = [];
+  obj.teamTasks = [];
   userRooms.push(obj);
+*/
 
   const eachRoom = userRooms.map((d) => {
     const roomName = Object.values(d)[0];
