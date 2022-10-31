05063296658c6794b330e85a843ac7152eb10bc1
Ryan Chang <chang774@purdue.edu>
2022-10-31 17:37:59 -0400

    Implemented very basic calendar deadlines
    Only accepts "yyyy-dd-mm", marked dates don't show corresponding task name(s)

--- client/components/CalendarView.jsx
+++ client/components/CalendarView.jsx
@@ -2,12 +2,71 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./TaskModal.css";
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+import "./Calendar.css";
+import Tasks from "./Task";
+
+//import "react-calendar/dist/Calendar.css";
 
 const CalendarView = (props) => {
   if (!props.show) {
     return null;
   }
+  let completedTasks = [];
+
+  var obj = new Object();
+  obj.name = "task-name";
+  obj.description = "task-desc";
+  obj.difficulty = "easy";
+  obj.deadline = "2022-10-03T00:00:00.000+00:00";
+  obj.points = 34;
+  obj.assigned = "prado156";
+  completedTasks.push(obj);
+
+  var obj2 = new Object();
+  obj2.name = "task-name";
+  obj2.description = "task-desc";
+  obj2.difficulty = "easy";
+  obj2.deadline = "2022-10-05T00:00:00.000+00:00";
+  obj2.points = 34;
+  obj2.assigned = "prado156";
+  completedTasks.push(obj2);
+
+
+  const eachTask = completedTasks.map((d) => {
+    const name = Object.values(d)[0];
+    const desc = Object.values(d)[1];
+    const difficulty = Object.values(d)[2];
+    const date = Object.values(d)[3];
+    const points = Object.values(d)[4];
+    const assigned = Object.values(d)[5];
+
+    return (
+      <Tasks
+        date={date}
+        desc={desc}
+        difficulty={difficulty}
+        assigned={assigned}
+        name={name}
+        points={points}
+      />
+    );
+  });
+  
+  const marked = new Set()
+/*
+  const onSelect=(e)=>{
+    const locale = 'fr-CA';
+    const date = new Intl.DateTimeFormat(
+      locale, 
+      {
+        year: "numeric", 
+        month: "2-digit", 
+        day: "2-digit"
+      }).format(e)
+      console.log(date);
+      console.log(marked.has(date));
+  }
+  */
 
   return (
     <div className="overlay" class="fixed pin z-50 overflow-auto flex">
@@ -17,7 +76,30 @@ const CalendarView = (props) => {
             Calendar
           </h1>
           <div className="grid justify-items-center">
-            <Calendar className="items-center" />
+            
+            <Calendar
+            tileClassName={({ date }) => {
+              const locale = 'fr-CA';
+              const dateConv = new Intl.DateTimeFormat(
+                locale, 
+                {
+                  year: "numeric", 
+                  month: "2-digit", 
+                  day: "2-digit"
+                }).format(date)
+                for (let val of eachTask.values()) {
+                  let deadline=val.props.date;
+                  if (!marked.has(deadline)){
+                    marked.add(deadline.slice(0,10));
+                  }
+                }
+              if(marked.has(dateConv)){
+               return  'highlight'
+              }
+            }}
+        
+            //onClickDay={(e)=>onSelect(e)}
+            />
           </div>
           {/* <h1 className="text-center text-8xl text-red-400	">Homepage</h1>; */}
           {/* <a href="/forgot_password" className="content-center text-8xl bg-red-400">
@@ -29,7 +111,7 @@ const CalendarView = (props) => {
             className="mt-4 group relative flex w-full justify-center rounded-md border border-transparent bg-red-500 py-2 px-4 text-sm font-medium text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             onClick={props.onClose}
           >
-            Abort
+            Close
           </button>
         </div>
       </div>
