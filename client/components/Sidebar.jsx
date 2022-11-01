9b0010677ea286664a9f966af62e7403e1253ff5
prado156 <ppattem@purdue.edu>
2022-11-01 13:50:57 -0400

    on click on room working

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -7,54 +7,43 @@ import JoinRoom from "./JoinRoom";
 const Sidebar = () => {
   const [showCreateRoom, setShowCreateRoom] = useState(false);
   const [showJoinRoom, setShowJoinRoom] = useState(false);
-  var dummyRoomJson = {
-    "_id": "635ece31760e04fe118bb4fd",
-    "name": "testRoom",
-    "owner": "635ecdd9760e04fe118bb4f9",
-    "type": "Democracy",
-    "users": [
-        "635ecdd9760e04fe118bb4f9"
-    ],
-    "__v": 46,
-    "assignedTasks": [],
-    "completedTasks": [],
-    "teamTasks": [],
-    "updatedAt": "2022-10-31T18:38:37.155Z"
-  }
-  dummyRoomJson = JSON.stringify(dummyRoomJson);
   let user_object = window.localStorage.getItem("user_data");
   user_object = JSON.parse(user_object);
-  user_object.rooms.push(dummyRoomJson);
-  console.log(user_object.rooms);
-  window.localStorage.setItem("user_data", JSON.stringify(user_object));
   let userRooms = user_object.rooms;
+  let roomId = "23234";
 
   // creating a temp room object, this will get replaced by the room array in the user object
-//  var obj = new Object();
-//  obj = JSON.parse(dummyRoomJson);
-
-/*
-  obj._id = "635ece31760e04fe118bb4fd";
-  obj.name = "testRoom";
-  obj.type = "Democracy";
-  obj.owner = "635ecdd9760e04fe118bb4f9";
-  obj.users = ["635ecdd9760e04fe118bb4f9", "63557b1340c7cd797a0ea840"];
-  obj.assignedTasks = [];
-  obj.completedTasks = [];
-  obj.teamTasks = [];
+  var obj = new Object();
+  obj.name = "Discord Room";
+  obj.type = "monarchy";
+  obj.roomId = "63477176250a07c21330fbe1";
   userRooms.push(obj);
-*/
+
+  var obj2 = new Object();
+  obj2.name = "Room";
+  obj2.type = "monarchy";
+  obj2.roomId = "13477176250a07c21330fbe1";
+  userRooms.push(obj2);
 
   const eachRoom = userRooms.map((d) => {
-    const roomName = Object.values(d)[0];
-    return <SideBarIcon icon={<FaFire size="60" />} text={roomName} />;
+    let roomName = Object.values(d)[0];
+    let roomId = Object.values(d)[2];
+    return (
+      <SideBarIcon icon={<FaFire size="60" />} text={roomId} id={roomId} />
+    );
   });
+
+  const roomSwitch = () => {
+    console.log("room switch");
+    // window.localStorage.removeItem("currentRoom");
+    // window.localStorage.setItem("currentRoom", id);
+  };
 
   return (
     <div className="fixed w-32 overflow-auto top-0 left-0 h-screen w-20 flex flex-col bg-gray-900 text-white ">
       <div>
         <div onClick={() => setShowCreateRoom(true)}>
-          <SideBarIcon icon={<BsPlus size="60" />} text="New Server" />
+          <SideBarIcon icon={<BsPlus size="60" />} text="New Room" />
         </div>
 
         <CreateRoom
@@ -65,16 +54,20 @@ const Sidebar = () => {
 
       <div>
         <div onClick={() => setShowJoinRoom(true)}>
-          <SideBarIcon icon={<BiCaretRight size="60" />} text="Join Room"/>
+          <SideBarIcon icon={<BiCaretRight size="60" />} text="Join Room" />
         </div>
 
-        <JoinRoom 
+        <JoinRoom
           onClose={() => setShowJoinRoom(false)}
           show={showJoinRoom}
-          ></JoinRoom>
+        ></JoinRoom>
+      </div>
+      <div>
+        <div onClick={roomSwitch}>
+          <a>{eachRoom}</a>
+        </div>
       </div>
 
-      {eachRoom}
       {/* <SideBarIcon icon={<FaFire size="60" />} /> */}
       <a href="/profile">
         <SideBarIcon icon={<BsGearFill size="60" />} text="Profile" />
