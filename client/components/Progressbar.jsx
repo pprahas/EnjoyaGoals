b7ac3797fcc042be315ce02e1a13a2e3dceaa927
prado156 <ppattem@purdue.edu>
2022-11-02 01:25:31 -0400

    progress bar works now along with number

--- client/components/Progressbar.jsx
+++ client/components/Progressbar.jsx
@@ -1,11 +1,13 @@
-
-const Progressbar = () => {
-  var w = ("percentageComp in localStorage")? window.localStorage.getItem("percentageComp"): 0;
+const Progressbar = (props) => {
+  var w = "percentageComp in localStorage"
+    ? window.localStorage.getItem("percentageComp")
+    : 0;
+  let number = window.localStorage.getItem("numberComp");
   return (
     <div className="flex flex-row w-fit bg-gray-200 rounded-full dark:bg-gray-700 mt-5">
       <div
         className="bg-green-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full h-4"
-        style={{ width: w+'%' }}
+        style={{ width: w + "%" }}
       >
         {" "}
         {w}%
--- server/routes/progress_bar.js
+++ server/routes/progress_bar.js
@@ -1,5 +1,7 @@
 const Room = require("../models/RoomModel");
 const express = require("express");
+
+const Task = require("../models/TaskModel");
 require("dotenv").config();
 
 const router = express.Router();
@@ -19,16 +21,24 @@ router.post("/", async (req, res) => {
     if (todo.length == 0) {
       res.status(200).send({ percent: percent });
     } else {
+      let incomplete_tasks = 0;
+      for (let i = 0; i < todo.length; i++) {
+        const task = await Task.findById(todo[i]);
+        if (task.status != "complete") {
+          incomplete_tasks += 1;
+        }
+      }
       let percent = (done.length / todo.length) * 100;
       percent = Math.floor(percent);
-      res.status(200).send({ percent: percent });
+
+      res.status(200).send({ percent: percent, number: incomplete_tasks });
     }
 
     /*
     const percent = (todoT / doneT) * 100;
     res.status(200).send({ percent: percent });*/
   } catch (error) {
-    res.status(400).send({ error: "Error occurred." });
+    res.status(400).send(error);
   }
 });
 
