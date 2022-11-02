ee2ab5513f9384beddb5917d1e81a23a950de3ed
prado156 <ppattem@purdue.edu>
2022-11-02 00:11:53 -0400

    progress bar works now

--- server/routes/progress_bar.js
+++ server/routes/progress_bar.js
@@ -8,25 +8,25 @@ router.use(express.json());
 
 router.post("/", async (req, res) => {
   try {
-    const todoT = await Room.findById(req.body.id)/*.select(todoTasks);*/
-    const doneT = await Room.findById(req.body.id)/*.select(
+    const todoT = await Room.findById(req.body.id); /*.select(todoTasks);*/
+    const doneT = await Room.findById(req.body.id); /*.select(
       completedTasks
     );*/
-   //console.log(doneT.get("completedTasks"));
+    //console.log(doneT.get("completedTasks"));
     const todo = todoT.get("teamTasks");
     const done = doneT.get("completedTasks");
     const percent = 100;
-    if(todo.length==0){
-      res.status(200).send({percent: percent});
+    if (todo.length == 0) {
+      res.status(200).send({ percent: percent });
     } else {
-      const percent = (done.length/todo.length) * 100;
-      res.status(200).send({percent: percent});
+      let percent = (done.length / todo.length) * 100;
+      percent = Math.floor(percent);
+      res.status(200).send({ percent: percent });
     }
 
-/*
+    /*
     const percent = (todoT / doneT) * 100;
     res.status(200).send({ percent: percent });*/
-    
   } catch (error) {
     res.status(400).send({ error: "Error occurred." });
   }
