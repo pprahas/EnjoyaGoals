6cb2f7cd5803f3114181f7c4124c20cb5f455a9e
prado156 <ppattem@purdue.edu>
2022-11-03 14:18:37 -0400

    updated progress bar again

--- server/routes/progress_bar.js
+++ server/routes/progress_bar.js
@@ -2,6 +2,7 @@ const Room = require("../models/RoomModel");
 const express = require("express");
 
 const Task = require("../models/TaskModel");
+const { tasks } = require("googleapis/build/src/apis/tasks");
 require("dotenv").config();
 
 const router = express.Router();
@@ -23,6 +24,9 @@ router.post("/", async (req, res) => {
       res.status(200).send({ percent: percent });
     } else {
       let incomplete_tasks = 0;
+      let complete_tasks = 0;
+      let tasks_left = 0;
+      let green_tasks = 0;
       for (let i = 0; i < todo.length; i++) {
         const task = await Task.findById(todo[i]);
         if (task.status != "complete") {
@@ -30,13 +34,24 @@ router.post("/", async (req, res) => {
         } else if (task.status != "missed") {
           complete_tasks += 1;
         }
+
+        if (task.status === "pending" || task.status === "unassigned") {
+          tasks_left += 1;
+        }
+
+        if (task.status === "complete") {
+          green_tasks += 1;
+        }
       }
 
       // let percent = (done.length / todo.length) * 100;
-      let percent = (complete_tasks/(incomplete_tasks + complete_tasks)) * 100;
+      let percent =
+        (complete_tasks / (incomplete_tasks + complete_tasks)) * 100;
+      let new_percent = (green_tasks / (green_tasks + tasks_left)) * 100;
       percent = Math.floor(percent);
 
-      res.status(200).send({ percent: percent, number: incomplete_tasks });
+      // res.status(200).send({ percent: percent, number: tasks_left });
+      res.status(200).send({ percent: new_percent, number: tasks_left });
     }
   } catch (error) {
     res.status(400).send(error);
