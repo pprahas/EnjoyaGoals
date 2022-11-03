0c0465ffb66ddce1d7717c8b6362787a646b4b1c
prado156 <ppattem@purdue.edu>
2022-11-03 17:46:43 -0400

    sorted leaderboard

--- server/routes/user.js
+++ server/routes/user.js
@@ -9,140 +9,172 @@ router.use(express.json());
 
 // route for updating a field of an existing user
 router.post("/update", async (req, res) => {
-	// this request should contain:
-	// "id":        the `_id` of the user,
-	// "fieldName": the name of the field to be changed,
-	// "value":     and the new value
-	const userToUpdate = await User.findById(req.body.id);
-	if (userToUpdate == null) {
-		console.log("Error at /user/update, userToUpdate is null");
-		console.log("req.body = ", req.body);
-		return res.status(500).json({ msg: "Updating user failed." });
-	}
+  // this request should contain:
+  // "id":        the `_id` of the user,
+  // "fieldName": the name of the field to be changed,
+  // "value":     and the new value
+  const userToUpdate = await User.findById(req.body.id);
+  if (userToUpdate == null) {
+    console.log("Error at /user/update, userToUpdate is null");
+    console.log("req.body = ", req.body);
+    return res.status(500).json({ msg: "Updating user failed." });
+  }
 
-	try {
-		switch (req.body.fieldName) {
-			case "rooms":
-				// to add or remove a room, request should also contain ONE of the following:
-				// "add":		true
-				// "remove":	true
-				if (req.body.add) { userToUpdate.rooms.push(req.body.value); }
-				else if (req.body.remove) {
-					console.log("userToUpdate = ", userToUpdate);
-					if (userToUpdate.rooms != null) {
-						userToUpdate.rooms.pull(req.body.value);
-					} else {
-						console.log("error updating User's rooms");
-						console.log("userToUpdate = ", userToUpdate);
-						return res.status(501).json({ msg: "Sorry, we messed up and can't do this operation right now. We'll fix it soon." });
-					}
-				}
-				else {
-					console.log("SOMETHING IS WRONG WITH THE /user/update ROUTE FOR UPDATING rooms");
-					return res.status(501).json({ msg: "Sorry, we messed up and can't do this operation right now. We'll fix it soon." });
-				}
-				break;
-		}
+  try {
+    switch (req.body.fieldName) {
+      case "rooms":
+        // to add or remove a room, request should also contain ONE of the following:
+        // "add":		true
+        // "remove":	true
+        if (req.body.add) {
+          userToUpdate.rooms.push(req.body.value);
+        } else if (req.body.remove) {
+          console.log("userToUpdate = ", userToUpdate);
+          if (userToUpdate.rooms != null) {
+            userToUpdate.rooms.pull(req.body.value);
+          } else {
+            console.log("error updating User's rooms");
+            console.log("userToUpdate = ", userToUpdate);
+            return res.status(501).json({
+              msg: "Sorry, we messed up and can't do this operation right now. We'll fix it soon.",
+            });
+          }
+        } else {
+          console.log(
+            "SOMETHING IS WRONG WITH THE /user/update ROUTE FOR UPDATING rooms"
+          );
+          return res.status(501).json({
+            msg: "Sorry, we messed up and can't do this operation right now. We'll fix it soon.",
+          });
+        }
+        break;
+    }
 
-		await userToUpdate.save();
-		return res.status(200).json({ msg: "User updated successfully." });
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ msg: "Updating user failed." });
-	}
+    await userToUpdate.save();
+    return res.status(200).json({ msg: "User updated successfully." });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Updating user failed." });
+  }
 });
 
 // route for updating a field of an existing user *THAT DIRECTLY RETURNS USER OBJECT RATHER THAN A MESSAGE
 router.post("/updateOM", async (req, res) => {
-	// this request should contain:
-	// "id":        the `_id` of the user,
-	// "fieldName": the name of the field to be changed,
-	// "value":     and the new value
-	const userToUpdate = await User.findById(req.body.id);
-	if (userToUpdate == null) {
-		console.log("Error at /user/update, userToUpdate is null");
-		console.log("req.body = ", req.body);
-		return res.status(500).json({ msg: "Updating user failed." });
-	}
+  // this request should contain:
+  // "id":        the `_id` of the user,
+  // "fieldName": the name of the field to be changed,
+  // "value":     and the new value
+  const userToUpdate = await User.findById(req.body.id);
+  if (userToUpdate == null) {
+    console.log("Error at /user/update, userToUpdate is null");
+    console.log("req.body = ", req.body);
+    return res.status(500).json({ msg: "Updating user failed." });
+  }
 
-	try {
-		switch (req.body.fieldName) {
-			case "rooms":
-				// to add or remove a room, request should also contain ONE of the following:
-				// "add":		true
-				// "remove":	true
-				if (req.body.add) { userToUpdate.rooms.push(req.body.value); }
-				else if (req.body.remove) {
-					console.log("userToUpdate = ", userToUpdate);
-					if (userToUpdate.rooms != null) {
-						userToUpdate.rooms.pull(req.body.value);
-					} else {
-						console.log("error updating User's rooms");
-						console.log("userToUpdate = ", userToUpdate);
-						return res.status(501).json({ msg: "Sorry, we messed up and can't do this operation right now. We'll fix it soon." });
-					}
-				}
-				else {
-					console.log("SOMETHING IS WRONG WITH THE /user/update ROUTE FOR UPDATING rooms");
-					return res.status(501).json({ msg: "Sorry, we messed up and can't do this operation right now. We'll fix it soon." });
-				}
-				break;
-		}
+  try {
+    switch (req.body.fieldName) {
+      case "rooms":
+        // to add or remove a room, request should also contain ONE of the following:
+        // "add":		true
+        // "remove":	true
+        if (req.body.add) {
+          userToUpdate.rooms.push(req.body.value);
+        } else if (req.body.remove) {
+          console.log("userToUpdate = ", userToUpdate);
+          if (userToUpdate.rooms != null) {
+            userToUpdate.rooms.pull(req.body.value);
+          } else {
+            console.log("error updating User's rooms");
+            console.log("userToUpdate = ", userToUpdate);
+            return res.status(501).json({
+              msg: "Sorry, we messed up and can't do this operation right now. We'll fix it soon.",
+            });
+          }
+        } else {
+          console.log(
+            "SOMETHING IS WRONG WITH THE /user/update ROUTE FOR UPDATING rooms"
+          );
+          return res.status(501).json({
+            msg: "Sorry, we messed up and can't do this operation right now. We'll fix it soon.",
+          });
+        }
+        break;
+    }
 
-		await userToUpdate.save();
+    await userToUpdate.save();
 
-		if (userToUpdate.rooms != null) {
-			// converting each Room _id in the User's rooms[] to a room object
-			const newRoomPromises = userToUpdate.rooms.map(convertID.convertRoomID);
-			const newRooms = await Promise.all(newRoomPromises);
-			// replace the rooms[] array with the array of Room Objects
-			userToUpdate.rooms = newRooms;
-			// *NOTE, DO NOT DO findRes.save() !!!!! WE DO NOT WANT TO
-			// REWRITE THE OBJECT IN THE DATABASE, WE WANT rooms[]
-			// TO STORE IDs IN THE DATABASE
+    if (userToUpdate.rooms != null) {
+      // converting each Room _id in the User's rooms[] to a room object
+      const newRoomPromises = userToUpdate.rooms.map(convertID.convertRoomID);
+      const newRooms = await Promise.all(newRoomPromises);
+      // replace the rooms[] array with the array of Room Objects
+      userToUpdate.rooms = newRooms;
+      // *NOTE, DO NOT DO findRes.save() !!!!! WE DO NOT WANT TO
+      // REWRITE THE OBJECT IN THE DATABASE, WE WANT rooms[]
+      // TO STORE IDs IN THE DATABASE
 
-			return res.status(200).json(userToUpdate);
-		} else {
-			console.log("error converting user's rooms");
-			console.log("userToUpdate = ", userToUpdate);
-			res.status(500).json({ msg: "User query failed." });
-		}
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ msg: "Updating user failed." });
-	}
+      return res.status(200).json(userToUpdate);
+    } else {
+      console.log("error converting user's rooms");
+      console.log("userToUpdate = ", userToUpdate);
+      res.status(500).json({ msg: "User query failed." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Updating user failed." });
+  }
 });
 router.post("/getRoomUsers", async (req, res) => {
-	const roomToGet = await Room.findById(req.body.id);
-	try {
-		let userNames = [];
+  const roomToGet = await Room.findById(req.body.id);
+  try {
+    let userNames = [];
+    let user_points_map = {};
 
-		for (let i = 0; i < roomToGet.users.length; i++) {
-			const user = await (User.findById(roomToGet.users[i]));
-			let levelname = "1 " + user.username;
-			if (!userNames.includes(levelname)) {
-				
-				let pointsEarned;
-				/*
+    for (let i = 0; i < roomToGet.users.length; i++) {
+      const user = await User.findById(roomToGet.users[i]);
+      let levelname = "1 " + user.username;
+      if (!userNames.includes(levelname)) {
+        let pointsEarned = 0;
+        /*
 				if (user.pointsEarned.has(req.body.id)) {
 					pointsEarned = user.pointsEarned.get(req.body.id);
 				} else {
 					pointsEarned = 0;
 				}
 				*/
-				//let level = Math.floor(pointsEarned/100)+1;
-				//let st = level.toString() + " "  + user.username; 
-				//userNames.push(st);
-				
-				userNames.push(levelname);
-			}
-		}
+        let level = Math.floor(pointsEarned / 100) + 1;
+        let st = level.toString() + " " + user.username;
+        userNames.push(st);
 
-		res.send(userNames);
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ msg: "Getting user failed." });
-	}
+        // user_points_map.set(user.username, level);
+
+        userNames.push(levelname);
+      }
+
+      user_points_map = {
+        prado156: 34,
+        p: 1000,
+        ryan: 213,
+      };
+
+      var array = [];
+      for (var key in user_points_map) {
+        array.push({
+          name: key,
+          value: user_points_map[key],
+        });
+      }
+
+      var sorted = array.sort(function (a, b) {
+        return a.value < b.value ? 1 : b.value < a.value ? -1 : 0;
+      });
+    }
+
+    res.status(200).send(sorted);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Getting user failed." });
+  }
 });
 
 module.exports = router;
