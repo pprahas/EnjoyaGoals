071bda49bde9fb16ec79e3d9e0c584cecffdc86f
Nick Norton <nortonn@purdue.edu>
2022-11-04 11:46:54 -0400

    Real lvl shows on profile

--- client/pages/Profile.jsx
+++ client/pages/Profile.jsx
@@ -14,6 +14,11 @@ export default function Profile(props){
   const lastName = user_object.lastName;
   const email = user_object.email;
   const username = user_object.username;
+  const pointsEarnedMap = new Map(Object.entries(user_object.pointsEarned));
+  const points = pointsEarnedMap.get(window.localStorage.getItem("currentRoom").toString());
+  const lvl = (Math.floor(points / 100)) + 1;
+  
+  console.log(`lvl = ${lvl}`);
   
   return (
 
@@ -89,7 +94,7 @@ export default function Profile(props){
                     border-4 border-indigo-300 dark:border-gray-800 rounded-full z-10 text-blueGray-500 font-bold uppercase"> */}
                     <span className="mt-16 lg:mt-14 leading-none items-center justify-center text-center flex inset-x-0 w-12 h-12 lg:-ml-3  bg-white 
                     border-4 border-indigo-300 dark:border-gray-800 rounded-full z-10 text-blueGray-500 font-bold uppercase">
-                      Lv.<br></br>34</span>
+                      Lv.<br></br>{lvl}</span>
 
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
