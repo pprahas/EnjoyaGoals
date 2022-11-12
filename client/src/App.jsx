4022203a5db4a25fa4b97cce49f43000d80244d9
prado156 <ppattem@purdue.edu>
2022-11-12 16:31:20 -0500

    found scuffed page, might change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "../components/Footer/Footer";
 import ProfileInformation from "../pages/ProfileInformation";
 import Profile from "../pages/Profile";
 import Homepage from "../pages/Homepage";
+import LandingPage from "../pages/LandingPage";
 import ResetPassword from "../pages/ResetPassword";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -18,7 +19,8 @@ function App() {
           path="/login"
           element={loggedIn ? <Homepage /> : <Login />}
         ></Route>
-        <Route path="/" element={loggedIn ? <Homepage /> : <Login />}></Route>
+        {/* <Route path="/" element={loggedIn ? <Homepage /> : <Login />}></Route> */}
+        <Route path="/" element={<LandingPage />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/forgot_password" element={<ForgotPassword />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
