00f242fe316fa9260fcffd63bdc647a77de6eee4
prado156 <ppattem@purdue.edu>
2022-11-26 14:46:06 -0500

    fixed merge conflicts

--- client/pages/Profile.jsx
+++ client/pages/Profile.jsx
@@ -53,17 +53,16 @@ export default function Profile(props) {
   );
   const lvl = Math.floor(points / 100) + 1;
 
-  const [roomName, setRoomName] = useState("Room Name");
+  // const [roomName, setRoomName] = useState("Room Name");
 
-  const roomId = window.localStorage.getItem("currentRoom");
-  const [roomName, setRoomName] = useState("Room Name")
+  // const roomId = window.localStorage.getItem("currentRoom");
+  const [roomName, setRoomName] = useState("Room Name");
   const [easy, setEasy] = useState(0);
   const [medium, setMedium] = useState(0);
   const [hard, setHard] = useState(0);
   const [total, setTotal] = useState(0);
   const [completeCount, setCompleteCount] = useState(0);
   const [pendingTaskCount, setPendingCount] = useState(0);
-
 
   useEffect(() => {
     getRoom();
@@ -83,49 +82,48 @@ export default function Profile(props) {
       });
   };
 
-
-    axios.post("http://localhost:8080/task/task_count", {
+  axios
+    .post("http://localhost:8080/task/task_count", {
       id: roomId,
       username: username,
-    }).then((res) => {
+    })
+    .then((res) => {
       //may have to change to UID when username changes are implemented
       setPendingCount(res.data[0]);
       setCompleteCount(res.data[3]);
+    });
+  //console.log(user_object);
+  axios
+    .post("http://localhost:8080/task/completed_tasks_personal", {
+      id: roomId,
+      UID: user_object._id,
     })
-    //console.log(user_object);
-    axios
-      .post("http://localhost:8080/task/completed_tasks_personal", {
-        id: roomId,
-        UID: user_object._id,
-      })
-      .then((res) => {
-        let points = 0;
-        let easyCount = 0;
-        let medCount = 0;
-        let hardCount = 0;
-        for (let i = 0; i < res.data.length; i++) {
-          let task = res.data[i];
-          if (task.status != "missed") {
-
-            points += task.points
-            if (task.difficulty == "Easy") {
-              easyCount += 1;
-            } else if (task.difficulty == "Medium") {
-              medCount += 1;
-            } else if (task.difficulty == "Hard") {
-              hardCount += 1;
-            }
+    .then((res) => {
+      let points = 0;
+      let easyCount = 0;
+      let medCount = 0;
+      let hardCount = 0;
+      for (let i = 0; i < res.data.length; i++) {
+        let task = res.data[i];
+        if (task.status != "missed") {
+          points += task.points;
+          if (task.difficulty == "Easy") {
+            easyCount += 1;
+          } else if (task.difficulty == "Medium") {
+            medCount += 1;
+          } else if (task.difficulty == "Hard") {
+            hardCount += 1;
           }
         }
-        setEasy(easyCount);
-        setMedium(medCount);
-        setHard(hardCount);
-        setTotal(points);
-      })
-      .catch((err) => {
-        console.log("error", err);
-      });
-  }
+      }
+      setEasy(easyCount);
+      setMedium(medCount);
+      setHard(hardCount);
+      setTotal(points);
+    })
+    .catch((err) => {
+      console.log("error", err);
+    });
 
   console.log(`lvl = ${lvl}`);
 
@@ -214,7 +212,6 @@ export default function Profile(props) {
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                     <div className="py-6 px-3 mt-32 sm:mt-0">
                       <a
-
                         className="bg-red-500 active:bg-red-700 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                         href="/homepage"
                       >
