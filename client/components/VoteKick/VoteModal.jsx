1def04ed6991ec5c0b56317387fe0d1bf08dfd63
prado156 <ppattem@purdue.edu>
2022-11-30 00:04:03 -0500

    added window reload

--- client/components/VoteKick/VoteModal.jsx
+++ client/components/VoteKick/VoteModal.jsx
@@ -5,125 +5,124 @@ import axios from "axios";
 //vote to kick any members of the room
 
 const VoteModal = (props) => {
-    if (!props.show) {
-        return null;
-    }
+  if (!props.show) {
+    return null;
+  }
 
-    const [userData, setUsers] = useState([]);
+  const [userData, setUsers] = useState([]);
 
-    const User = (props) => (
-        <span
-            className="justify-center pb-2 w-full mt-2 mb-2 bg-white items-center text-white leading-none rounded-md flex "
-        >
-            <span className="ml-12 w-24 text-center rounded-md bg-indigo-300 uppercase px-2 py-1 text-xs font-bold mr-3">
-                {props.name}
-            </span>
+  const User = (props) => (
+    <span className="justify-center pb-2 w-full mt-2 mb-2 bg-white items-center text-white leading-none rounded-md flex ">
+      <span className="ml-12 w-24 text-center rounded-md bg-indigo-300 uppercase px-2 py-1 text-xs font-bold mr-3">
+        {props.name}
+      </span>
 
+      <button
+        className="flex rounded-full bg-red-600 uppercase px-2 py-1 text-xs font-bold mr-3"
+        onClick={() => {
+          updateVotes(props.UID, true);
+        }}
+      >
+        +
+      </button>
+      <button
+        className="flex rounded-full  bg-green-600 uppercase px-2 py-1 text-m font-extrabold mr-4"
+        onClick={() => {
+          updateVotes(props.UID, false);
+        }}
+      >
+        -
+      </button>
+      <button className="flex rounded-md text-gray-700 bg-white uppercase px-2 py-1 text-m font-medium mr-12 border border-gray-300">
+        {props.votes}
+      </button>
+    </span>
+  );
 
-            <button className="flex rounded-full bg-red-600 uppercase px-2 py-1 text-xs font-bold mr-3"
-                onClick={() => {
-                    updateVotes(props.UID, true)
-                }}>
-                +
-            </button>
-            <button className="flex rounded-full  bg-green-600 uppercase px-2 py-1 text-m font-extrabold mr-4"
-                onClick={() => {
-                    updateVotes(props.UID, false)
-                }}>
-                -
-            </button>
-            <button className="flex rounded-md text-gray-700 bg-white uppercase px-2 py-1 text-m font-medium mr-12 border border-gray-300">
-                {props.votes}
-            </button>
-        </span>
-    );
+  const updateVotes = async (UID, action) => {
+    let roomId = props.data;
+    axios
+      .post("http://localhost:8080/vote/kick/team_member", {
+        roomId: roomId,
+        userId: UID,
+        action: action,
+      })
+      .then((res) => {
+        console.log(res);
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log("error", err);
+        window.location.reload();
+      });
+  };
+  const getUsers = async () => {
+    axios
+      .post("http://localhost:8080/vote/show/team_member", {
+        roomId: props.data,
+      })
+      .then((res) => {
+        let users = [];
+        console.log(res.data);
 
-    const updateVotes = async (UID, action) => {
-       
-        let roomId = props.data;
-        axios.post("http://localhost:8080/vote/kick/team_member", {
-            roomId:roomId,
-            userId:UID,
-            action:action
-        }).then((res) => {
-            console.log(res)
+        for (const [key, value] of Object.entries(res.data)) {
+          //console.log(key, value);
+          let UID = key;
+          let name = value.split("!@#$")[0];
+          let votes = value.split("!@#$")[1];
+          users.push(<User name={name} UID={UID} votes={votes} />);
         }
-        ).catch((err) => {
-            console.log("error", err)
-        }
-        );
-    }
-    const getUsers = async () => {
-        axios
-            .post("http://localhost:8080/vote/show/team_member", {
-                roomId: props.data,
-            })
-            .then((res) => {
-                let users = [];
-                console.log(res.data);
-                
-                for (const [key, value] of Object.entries(res.data)) {
-                    //console.log(key, value);
-                    let UID = key;
-                    let name = value.split("!@#$")[0]
-                    let votes = value.split("!@#$")[1]
-                    users.push(<User name={name} UID={UID} votes={votes} />)
-                }
-                setUsers(users);
-            })
-            .catch((err) => {
-                console.log("error", err);
-            });
-    };
-    useEffect(() => {
-        getUsers();
+        setUsers(users);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return (
+    <div className="overlay" class="fixed pin z-50 overflow-auto flex">
+      <div className="modal" onClick={props.onClose}>
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <div className="modal-header">
+            <h4
+              className="modal-title"
+              class="text-center font-medium text-2xl leading-4 text-gray-700 mb-4 mt-4 "
+            >
+              Choose someone to vote out.
+            </h4>
+            <h5
+              className="modal-title"
+              class="text-center font-medium text-xl leading-4 text-gray-700 mb-4 mt-4 "
+            >
+              (+) = vote out, (-) = keep in
+            </h5>
+            <h6
+              className="modal-title"
+              class="text-center font-medium text-l leading-4 text-gray-700 mb-4 mt-4 "
+            >
+              X Votes to Kick
+            </h6>
+          </div>
 
-    }, []);
-    return (
-        <div className="overlay" class="fixed pin z-50 overflow-auto flex">
-            <div className="modal" onClick={props.onClose}>
-                <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                    <div className="modal-header">
-                        <h4
-                            className="modal-title"
-                            class="text-center font-medium text-2xl leading-4 text-gray-700 mb-4 mt-4 "
-                        >
-                            Choose someone to vote out.
-                        </h4>
-                        <h5
-                            className="modal-title"
-                            class="text-center font-medium text-xl leading-4 text-gray-700 mb-4 mt-4 "
-                        >
-                            (+) = vote out, (-) = keep in
-                        </h5>
-                        <h6
-                            className="modal-title"
-                            class="text-center font-medium text-l leading-4 text-gray-700 mb-4 mt-4 "
-                        >
-                            X Votes to Kick
-                        </h6>
-                    </div>
-
-                    <div className=" items-center justify-center w-32 overflow-auto w-full w-20 flex bg-white text-white border-gray-100 border-t border-b">
-                        <div className="modal-body">
-                            {userData}
-                        </div>
-                    </div>
-                    <div className="modal-footer">
-
-                        <button
-                            className="button"
-                            onClick={props.onClose}
-                            class="ml-2 mb-3 rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium leading-4 text-gray-700 
+          <div className=" items-center justify-center w-32 overflow-auto w-full w-20 flex bg-white text-white border-gray-100 border-t border-b">
+            <div className="modal-body">{userData}</div>
+          </div>
+          <div className="modal-footer">
+            <button
+              className="button"
+              onClick={props.onClose}
+              class="ml-2 mb-3 rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium leading-4 text-gray-700 
                         shadow-sm hover:bg-gray-50 focus:outline-none"
-                        >
-                            Close
-                        </button>
-                    </div>
-                </div>
-            </div>
+            >
+              Close
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default VoteModal;
