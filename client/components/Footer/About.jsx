cd7d4875c3e34d0361dce77c8b2df2b4a1270147
Ryan Chang <chang774@purdue.edu>
2022-12-01 23:55:46 -0500

    Adjusted CSS

--- client/components/Footer/About.jsx
+++ client/components/Footer/About.jsx
@@ -3,29 +3,29 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div className="container">
-      <img src="logo.png" alt="logo" className="AboutCenter" style={{ textAlign:"center" }}></img>
+    <div className="mt-40 text-center justify-center">
+      <img src="logo.png" alt="logo" className="contact-image" />
       {/* border: "1px red solid", */}
-        <div style={{textAlign: "center", marginTop: 170, fontSize: 18, width: "100%", marginLeft: 80}}>
-        
-          <h2 className="textColor">
-          <b style={{fontSize: 20, marginBottom: 10}}>Purpose:</b>
-          </h2>
-          <p style={{textAlign: "center"}}>
-          As society has progressed, we have developed ways to make structuring projects easier but not necessarily more fun. 
-          
-          Why not? Nearly everything in people’s lives requires some amount of planning. Although there are existing services that allow you to organize your 
-          goals, they fail to take advantage of people’s desire to receive instant gratification and to have fun. 
+      <div className="text-lg px-40 mt-12" /*style={{textAlign: "center", marginTop: 170, fontSize: 18, width: "100%", marginLeft: 80}}*/>
+
+        <h2 className="textColor">
+          <b className="text-xl">Purpose:</b>
+        </h2>
+        <p style={{ textAlign: "center" }}>
+          As society has progressed, we have developed ways to make structuring projects easier but not necessarily more fun.
+
+          Why not? Nearly everything in people’s lives requires some amount of planning. Although there are existing services that allow you to organize your
+          goals, they fail to take advantage of people’s desire to receive instant gratification and to have fun.
           Our goal is to create a service that allows people to plan their goals in a fun and engaging way.
 
           We wanted to create a platform that would help people stay motivated and on track with their goals by utilizing video game environment which capitilizes our desire for immediate rewards, pleasure, and feedback.
           We hope that this app will help people achieve their goals and become better versions of themselves.
 
         </p>
-        
+
       </div>
-      <div style={{ textAlign: "center", marginTop: 80, fontSize: 18, width: "100%", marginLeft: 80 }}>
-        <h2 className="textColor"><b style={{fontSize: 20}}>Our Developers:</b></h2>
+      <div className="text-lg px-40 mt-12" >
+        <h2 className="textColor"><b className="text-xl">Our Developers:</b></h2>
         <p>
           Ryan Chang, Prahas Pattem, Nabi Nabiyev, Karim Mammadli, Nick Norton
         </p>
--- client/components/Footer/Contact/Contact.jsx
+++ client/components/Footer/Contact/Contact.jsx
@@ -4,42 +4,42 @@ import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div className="container">
+    <div className="mt-40 text-center justify-center">
       <img src="logo.png" alt="logo" className="contact-image"></img>
-      
-      <div style={{textAlign: "center", marginTop: 90, fontSize: 18, marginLeft: 80}}>
-      
-        <h2 className="textColor">
-        <b style={{fontSize: 20, marginBottom: 10}}>Contact Us:</b>
-        </h2>
-        <p style={{textAlign: "center"}}>
-        If you have any questions or concerns, please contact us at:
-        <br></br>
-        <br></br>
-        <a href="mailto:
-        ">Email: <br></br><u>Enjoyagoals@gmail.com</u></a>
-        <br></br>
-        <br></br>
-        Enjoyagoals: <a href="https://github.com/pprahas/Enjoyagoals
-        "><u style={{marginBottom: 80}}> Github</u></a>
-        <br></br>
-        Prahas Pattem: <a href="https://github.com/pprahas"><u>Github</u></a>  <a href="https://www.linkedin.com/in/pprahas/"><u>LinkedIn</u></a> 
-        <br></br>
 
-        Karim Mammadli: <a href="https://github.com/Karim-Mammadli"><u>Github</u></a> <a href="https://www.linkedin.com/in/karimmammadli"><u>LinkedIn</u></a> 
-        <br></br>
-        Ryan Chang: <a href="https://github.com/nayr-gnahc"><u>Github</u></a>
-        <br></br>
-        Nabi Nabiyev: <a href="https://github.com/mikewazovski"><u>Github</u></a> <a href="https://www.linkedin.com/in/nabi-nabiyev-646736132"><u>LinkedIn</u></a> 
-        <br></br>
-        Nick Norton: <a href="https://github.com/NickNort"><u>Github</u></a> <a href="https://www.linkedin.com/in/nicknort/"><u>LinkedIn</u></a> 
-        <br></br>
-       
-      </p>
-      
-    </div>
+      <div className="inset-x-0 fixed mt-20"/*style={{textAlign: "center", marginTop: 90, fontSize: 18, marginLeft: 80}}*/>
+
+        <h2 className="text-center justify-center">
+          <b className="textColor" /*style={{fontSize: 20, marginBottom: 10}}*/>Contact Us:</b>
+        </h2>
+        <p style={{ textAlign: "center" }}>
+          If you have any questions or concerns, please contact us at:
+          <br></br>
+          <br></br>
+          <a href="mailto:
+        ">Email: <br></br><u>Enjoyagoals@gmail.com</u></a>
+          <br></br>
+          <br></br>
+          Enjoyagoals: <a href="https://github.com/pprahas/Enjoyagoals
+        "><u style={{ marginBottom: 80 }}> Github</u></a>
+          <br></br>
+          Prahas Pattem: <a href="https://github.com/pprahas"><u>Github</u></a>  <a href="https://www.linkedin.com/in/pprahas"><u>LinkedIn</u></a>
+          <br></br>
+
+          Karim Mammadli: <a href="https://github.com/Karim-Mammadli"><u>Github</u></a> <a href="https://www.linkedin.com/in/karimmammadli"><u>LinkedIn</u></a>
+          <br></br>
+          Ryan Chang: <a href="https://github.com/nayr-gnahc"><u>Github</u></a>
+          <br></br>
+          Nabi Nabiyev: <a href="https://github.com/mikewazovski"><u>Github</u></a> <a href="https://www.linkedin.com/in/nabi-nabiyev-0b0b0b0b0"><u>LinkedIn</u></a>
+          <br></br>
+          Nick Norton: <a href="https://github.com/NickNort"><u>Github</u></a> <a href="https://www.linkedin.com/in/nick-norton-0b0b0b0b0"><u>LinkedIn</u></a>
+          <br></br>
+
+        </p>
 
       </div>
+
+    </div>
   );
 };
 
