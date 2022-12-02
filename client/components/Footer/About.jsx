1d9f15cc6c7c0fc78c4af0a079aca701f8f9d87f
Karim Mammadli <kmammadl@purdue.edu>
2022-12-01 23:31:41 -0500

    About & Contact

--- client/components/Footer/About.jsx
+++ client/components/Footer/About.jsx
@@ -1,8 +1,31 @@
+import "../../public/logo.png";
+import "./About.css";
+
 const About = () => {
   return (
     <div className="container">
-      <div style={{ textAlign: "center" }}>
-        <h2>Our Developers:</h2>
+      <img src="logo.png" alt="logo" className="AboutCenter" style={{ textAlign:"center" }}></img>
+      {/* border: "1px red solid", */}
+        <div style={{textAlign: "center", marginTop: 170, fontSize: 18, width: "100%", marginLeft: 80}}>
+        
+          <h2 className="textColor">
+          <b style={{fontSize: 20, marginBottom: 10}}>Purpose:</b>
+          </h2>
+          <p style={{textAlign: "center"}}>
+          As society has progressed, we have developed ways to make structuring projects easier but not necessarily more fun. 
+          
+          Why not? Nearly everything in people’s lives requires some amount of planning. Although there are existing services that allow you to organize your 
+          goals, they fail to take advantage of people’s desire to receive instant gratification and to have fun. 
+          Our goal is to create a service that allows people to plan their goals in a fun and engaging way.
+
+          We wanted to create a platform that would help people stay motivated and on track with their goals by utilizing video game environment which capitilizes our desire for immediate rewards, pleasure, and feedback.
+          We hope that this app will help people achieve their goals and become better versions of themselves.
+
+        </p>
+        
+      </div>
+      <div style={{ textAlign: "center", marginTop: 80, fontSize: 18, width: "100%", marginLeft: 80 }}>
+        <h2 className="textColor"><b style={{fontSize: 20}}>Our Developers:</b></h2>
         <p>
           Ryan Chang, Prahas Pattem, Nabi Nabiyev, Karim Mammadli, Nick Norton
         </p>
@@ -10,5 +33,6 @@ const About = () => {
     </div>
   );
 };
+
 
 export default About;
--- client/components/Footer/Contact/Contact.jsx
+++ client/components/Footer/Contact/Contact.jsx
@@ -1,52 +1,45 @@
 // import "./Contact.css"; // Tell webpack that Button.js uses these styles
+import "../../../public/logo.png";
+import "./Contact.css";
 
 const Contact = () => {
   return (
     <div className="container">
-      <div style={{ textAlign: "center" }}>
-        <h2>Contact Us</h2>
-        <p>Swing by for a cup of coffee, or leave us a message:</p>
-      </div>
-      <div className="row">
-        <div className="column">
-          {/* <img src="/logo.png" style={{ width: "100%" }} /> */}
-        </div>
-        <h4 className="title">enjoyagoals@gmail.com</h4>
-        {/* <div className="column">
-          <form action="/action_page.php">
-            <label htmlFor="fname">First Name</label>
-            <input
-              type="text"
-              id="fname"
-              name="firstname"
-              placeholder="Your name.."
-            />
-            <label htmlFor="lname">Last Name</label>
-            <input
-              type="text"
-              id="lname"
-              name="lastname"
-              placeholder="Your last name.."
-            />
-            <label htmlFor="country">Country</label>
-            <select id="country" name="country">
-              <option value="australia">Australia</option>
-              <option value="canada">Canada</option>
-              <option value="usa">USA</option>
-            </select>
-            <label htmlFor="subject">Subject</label>
-            <textarea
-              id="subject"
-              name="subject"
-              placeholder="Write something.."
-              style={{ height: 170 }}
-              defaultValue={""}
-            />
-            <input type="submit" defaultValue="Submit" />
-          </form>
-        </div> */}
-      </div>
+      <img src="logo.png" alt="logo" className="contact-image"></img>
+      
+      <div style={{textAlign: "center", marginTop: 90, fontSize: 18, marginLeft: 80}}>
+      
+        <h2 className="textColor">
+        <b style={{fontSize: 20, marginBottom: 10}}>Contact Us:</b>
+        </h2>
+        <p style={{textAlign: "center"}}>
+        If you have any questions or concerns, please contact us at:
+        <br></br>
+        <br></br>
+        <a href="mailto:
+        ">Email: <br></br><u>Enjoyagoals@gmail.com</u></a>
+        <br></br>
+        <br></br>
+        Enjoyagoals: <a href="https://github.com/pprahas/Enjoyagoals
+        "><u style={{marginBottom: 80}}> Github</u></a>
+        <br></br>
+        Prahas Pattem: <a href="https://github.com/pprahas"><u>Github</u></a>  <a href="https://www.linkedin.com/in/pprahas"><u>LinkedIn</u></a> 
+        <br></br>
+
+        Karim Mammadli: <a href="https://github.com/Karim-Mammadli"><u>Github</u></a> <a href="https://www.linkedin.com/in/karimmammadli"><u>LinkedIn</u></a> 
+        <br></br>
+        Ryan Chang: <a href="https://github.com/nayr-gnahc"><u>Github</u></a> <a href="https://www.linkedin.com/in/ryan-chang-1b1b3b1b3"><u>LinkedIn</u></a> 
+        <br></br>
+        Nabi Nabiyev: <a href="https://github.com/mikewazovski"><u>Github</u></a> <a href="https://www.linkedin.com/in/nabi-nabiyev-0b0b0b0b0"><u>LinkedIn</u></a> 
+        <br></br>
+        Nick Norton: <a href="https://github.com/NickNort"><u>Github</u></a> <a href="https://www.linkedin.com/in/nick-norton-0b0b0b0b0"><u>LinkedIn</u></a> 
+        <br></br>
+       
+      </p>
+      
     </div>
+
+      </div>
   );
 };
 
