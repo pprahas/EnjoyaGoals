dcb82a0ef8657014fe9bc0df84712cca6930808b
Karim Mammadli <kmammadl@purdue.edu>
2022-12-01 23:44:55 -0500

    fixed contacts

--- client/components/Footer/Contact/Contact.jsx
+++ client/components/Footer/Contact/Contact.jsx
@@ -23,16 +23,16 @@ const Contact = () => {
         Enjoyagoals: <a href="https://github.com/pprahas/Enjoyagoals
         "><u style={{marginBottom: 80}}> Github</u></a>
         <br></br>
-        Prahas Pattem: <a href="https://github.com/pprahas"><u>Github</u></a>  <a href="https://www.linkedin.com/in/pprahas"><u>LinkedIn</u></a> 
+        Prahas Pattem: <a href="https://github.com/pprahas"><u>Github</u></a>  <a href="https://www.linkedin.com/in/pprahas/"><u>LinkedIn</u></a> 
         <br></br>
 
         Karim Mammadli: <a href="https://github.com/Karim-Mammadli"><u>Github</u></a> <a href="https://www.linkedin.com/in/karimmammadli"><u>LinkedIn</u></a> 
         <br></br>
-        Ryan Chang: <a href="https://github.com/nayr-gnahc"><u>Github</u></a> <a href="https://www.linkedin.com/in/ryan-chang-1b1b3b1b3"><u>LinkedIn</u></a> 
+        Ryan Chang: <a href="https://github.com/nayr-gnahc"><u>Github</u></a>
         <br></br>
-        Nabi Nabiyev: <a href="https://github.com/mikewazovski"><u>Github</u></a> <a href="https://www.linkedin.com/in/nabi-nabiyev-0b0b0b0b0"><u>LinkedIn</u></a> 
+        Nabi Nabiyev: <a href="https://github.com/mikewazovski"><u>Github</u></a> <a href="https://www.linkedin.com/in/nabi-nabiyev-646736132"><u>LinkedIn</u></a> 
         <br></br>
-        Nick Norton: <a href="https://github.com/NickNort"><u>Github</u></a> <a href="https://www.linkedin.com/in/nick-norton-0b0b0b0b0"><u>LinkedIn</u></a> 
+        Nick Norton: <a href="https://github.com/NickNort"><u>Github</u></a> <a href="https://www.linkedin.com/in/nicknort/"><u>LinkedIn</u></a> 
         <br></br>
        
       </p>
